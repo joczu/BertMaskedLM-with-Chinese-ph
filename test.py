a7603b948156e66941ef7f98b96e0f26817d5be3
spzhuang <694789266@qq.com>
2021-08-05 17:26:49 +0800

    Update test.py
    aaa

--- test.py
+++ test.py
@@ -4,186 +4,257 @@
 #   Copyright (C) 2019 Tuya NLP. All rights reserved.
 #   
 #   FileName：test.py
-#   Author  ：rentc(桑榆)
-#   DateTime：2021/4/24
-#   Desc    ：
+#   Author  ：shizi
+#   DateTime：2021/7/15
+#   Desc    ：对训练好的模型进行效果测试，能够统一Bert和Gpt2模型测试。
 #
 # ================================================================
 import torch
 import logging
 import argparse
-import re
-from transformers import AdamW
 import torch.utils.data as Data
-from data_process import MyDataset
-from tokenizer.tokenizer import MyTokenizer
-from transformers import BertModel, BertConfig
-from transformers import BertTokenizer, BertForMaskedLM
+from datetime import datetime
+from transformers import GPT2LMHeadModel,BertForMaskedLM
+from utils import (calculate_loss_and_accuracy,GptTokenTool,
+                  BertTokenTool,MyDataset,top_accuracy,day_month)
+from utils import ppl_based_union_ngrams_lm as perplexity
+from utils import calculate_last_logits_and_loss as cllal
+from sklearn import decomposition
+from torch.utils.tensorboard import SummaryWriter
+import tensorboard as tb
+import tensorflow as tf
+import numpy as np
+tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 import os
 import time
-import math
 import pdb
-from scipy import sparse
 
 def parser_args():
     """
     :return: args
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--test_data_path', type=str, default="./test/SougouTest.txt", help='test data path')
-    parser.add_argument('-v', '--vocab_path', type=str, default='./vocab/SougouBertVocab.txt', help='vocab file path')
-    parser.add_argument('-b', '--batch_size', type=int, default=4, help='batch_size')
-    parser.add_argument('--each_steps', type=int, default=100, help='save model after train N steps ')
-    parser.add_argument('--usegpu', action='store_true',default=False, help='use GPU or not')
-    parser.add_argument('--device', type=str, default=1)
-    parser.add_argument('--load_model', type=str, default='')
-    parser.add_argument('--log_path', type=str, default='')
-    parser.add_argument('--perplexity',type=str,default='')
-    parser.add_argument('--lastone_token',action='store_true', default=False,help='let the last word in a sentence be [MASK]')
+    parser.add_argument('--modelname',type=str,default='bert',
+                        help='input your test model name, bert or gpt2')
+    parser.add_argument('--loadmodel', type=str, 
+                        default='./modelfile/bert/bert_sougou_word_step_1000.bin',
+                        help="load trained model for test its profiler.")
+    parser.add_argument('--testfile', type=str, 
+                        default="./testdata/stard1w.txt", 
+                        help='test data path')
+    parser.add_argument('--vocabpath', type=str, 
+                        default='./vocab/SougouBertVocab.txt', 
+                        help='vocab file path')
+    parser.add_argument('--batchsize', type=int, default=2, help='batch_size')
+    parser.add_argument('--showstep', type=int, default=100, help='save model after train N steps ')
+    parser.add_argument('--usegpu', type=int,default=0, help='use GPU or not')
+    parser.add_argument('--device', type=str, default='0',
+                        help="""if usegpu and only a sigle gpu, you can ignore 
+                        the item, but if you use multi gpu, you should set the 
+                        item='0,1' for two pieces of gpu, item='0,1,2' for 
+                        three pieces of gpu and so on""")
+    parser.add_argument('--log', type=str, default='./log/MN/CN_DT_test_log.txt')
+    parser.add_argument('--tensorboard',type=str,default='./tensorboard/MN/CN_DT_test',
+                        help='tensorboard directory')
+    parser.add_argument('--demodata',type=str,default='./test/demodata.xls',
+                        help='sure your demo data file address')
+    parser.add_argument('--save_demo',type=str,
+                        default='./demo/MN/MN_CN_MO_DT_L_perplexity.xls',
+                        help='''draw the (log) perplexity distribution''')
+    parser.add_argument('--isppl',type=int,default=0,
+                        help='whether or not calculate perplexity')
+    parser.add_argument('--ppltype',type=str,default='log',
+                        help='ppltype = log or ori')
+    parser.add_argument('--islastone',type=int,default=0,
+                        help='if test the generate lastone task')
     args = parser.parse_args()
     return args
 
 args = parser_args()
-if not args.load_model:
-    raise ValueError('test.py need a trained model')
-else:
-    model = BertForMaskedLM.from_pretrained(args.load_model)
-multi_gpu = False
+assert args.loadmodel,'test.py need a trained model'
+assert args.usegpu in [0,1],'usegpu is equal 0 or 1'
+assert args.isppl in [0,1], 'isppl is equal 0 or 1'
+assert args.islastone in [0,1], 'islastone is equal 0 or 1'
+assert args.modelname in args.loadmodel, "let your args.modelname in args.loadmodel"
 
-
-if args.perplexity:
-    para_matrix = sparse.dok_matrix(sparse.load_npz(args.perplexity))
-
+day = day_month(datetime.now())
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-# formatter = logging.Formatter(
-#     '%(asctime)s - %(levelname)s - %(message)s')
-
-# 创建一个handler，用于写入日志文件
-file_handler = logging.FileHandler(filename=args.log_path,mode='w')
-#file_handler.setFormatter(formatter)
+logfile = args.log
+corpusname = args.testfile.split('/')[-1]
+corpusname = corpusname.split('.')[0]
+logfile = logfile.replace('MN',args.modelname)
+logfile = logfile.replace('CN',corpusname).replace('DT',day)
+file_handler = logging.FileHandler(filename=logfile,mode='w')
 file_handler.setLevel(logging.INFO)
 logger.addHandler(file_handler)
-
-# 创建一个handler，用于将日志输出到控制台
 console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
-#console.setFormatter(formatter)
 logger.addHandler(console)
 
+tb_dir = args.tensorboard.replace('MN',args.modelname)
+tb_dir = tb_dir.replace('CN',corpusname).replace('DT',day)
+tb_writer = SummaryWriter(log_dir = tb_dir)
 
-if args.usegpu==True and args.device:
-    if len(args.device)==1:
-        device=torch.device(int(args.device))
+if args.modelname=='gpt2':
+    model = GPT2LMHeadModel.from_pretrained(args.loadmodel)
+    tokenizer = GptTokenTool(args.vocabpath)
+else:
+    model = BertForMaskedLM.from_pretrained(args.loadmodel)
+    tokenizer = BertTokenTool(args.vocabpath)
+    
+multi_gpu = False
+if bool(args.usegpu)==True and args.device:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    assert str(device)=='cuda','your machine need own a gpu card at least'
+    udevice = list(map(int,args.device.split(',')))
+    udevice = [i for i in udevice if type(i)==int]
+    if len(udevice)==1:
+        sdevice=torch.device(udevice[0])
+        model = model.to(sdevice)
+    elif len(udevice)>1 and torch.cuda.device_count()>1:
         model = model.to(device)
-    elif len(args.device)>1 and torch.cuda.device_count()>1:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model = model.to(device)
-        device_ids=[int(i) for i in args.device.split(',')]
+        device_ids=[int(i) for i in udevice]
         model = torch.nn.DataParallel(model,device_ids=device_ids)
         multi_gpu = True
 else:
     device = torch.device("cpu")
 
-test_dataset = MyDataset(args.test_data_path, n_raws=1000, shuffle=False)
-#pdb.set_trace()
 
-mt = MyTokenizer(args.vocab_path)
+testset = MyDataset(args.testfile, n_raws=500, shuffle=False)
 time0 = time.time()
-logger.info("The test model is : %s"%args.load_model)
-logger.info("The logger information is saved in : %s"%args.log_path)
+logger.info("Test is begining at %s"%day)
+logger.info("The test model is : %s"%args.loadmodel)
+logger.info("The logger information is saved in : %s"%logfile)
 
-step = 0    
-test_dataset.initial()
-total_samplers = len(test_dataset)
-test_iter = Data.DataLoader(dataset=test_dataset, batch_size=args.batch_size)
-correct_5 = 0
-correct_3 = 0
-correct_1 = 0
-prep_storge = []
-total = 0
+testset.initial()
+total_samplers = len(testset)
+test_iter = Data.DataLoader(dataset=testset, batch_size=args.batchsize)
 
-def top_accuracy(mask_token_logits,real_idx,k):
-    res = 0
-    word_idx = torch.topk(mask_token_logits,k).indices   # top5
-    for k2 in range(len(real_idx)):
-        if real_idx[k2].item() in word_idx[k2]:
-            res += 1
-    return res
-def perplexity(sen, logit, pm):
-    '''
-    sen : 经过bert分词化的句子，tensor格式
-    label : 标注句子中被mask的地方
-    pm : 参数矩阵
-    # ======
-    perplexity = (p(w_1|w_0) * p(w_2|w_1) * ... * p(w_n|w_{n-1}))^(-1/2)
-    '''
-    preplexity = 0
-    #pdb.set_trace()
-    sen_flag = sen != 103
-    logit = torch.argmax(logit,axis=-1)
-    fix_sen = torch.where(sen_flag, sen, logit)
-    fix_sen = fix_sen[fix_sen !=0 ]  # 去除补码位
-    for i in range(1,len(fix_sen)):
-        try:
-            value = math.log(pm[fix_sen[i-1],fix_sen[i]])
-        except ValueError:
-            value = -1000   # python中的log可达到的最小值为math.log(1e-323)=-743.74..再小就会变成0，变成math.log(0)引发错误。
-        preplexity += value
-    log_preplexity = preplexity*(-0.5)
-    return log_preplexity
-    
+
+#%% 增加句子表示功能  已完成，可视化有效
+# ori = '即想 办法 在 有限 时间 内 增加 运动 速度'
+# sen,_ = tokenizer.tokenize(ori, max_length=100)
+# output = model(sen)
+# logit = output.logits.squeeze()
+# sen_logit = logit.mean(0,keepdim=True)
+# sen_logit2 = (sen_logit + 0.1)/2
+# ssen = torch.cat([sen_logit,sen_logit2],dim=0)
+# tb_writer.add_embedding(ssen,['sentence1','sentence2'],global_step=1)
+# tb_writer.close()
+
+
+
+#%% 测试
+top5 = 0
+top3 = 0
+top1 = 0
+ppl = [] if args.isppl else None
+tot = 0
+
+#sentence_num = 1000
+PCA = decomposition.PCA(128)
+sen_vector = []
+sen_label = []
+transfer = tokenizer.tokenizer.convert_ids_to_tokens
+# 由于bert的masked是随机的，并且一个batch内有的sample可能有两个masked，有的可能只有一个，
+# 不好结构化，因此面对bert的topk测试，使用了枚举法。
+# bert的困惑度不好搞，目前不支持困惑度。
 for gg, data in enumerate(test_iter):
-    pdb.set_trace()
-    if not args.lastone_token:
-        inputs,labels = mt.tokenize(data, max_length=100, p_mask = 0.15)
-    else:
-        inputs,labels = mt.tokenize(data, max_length=100, p_mask = 0, islastone=True)
-    if args.usegpu==True and args.device:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-    outputs = model(**inputs, labels=labels)
-    loss = outputs.loss.mean() if multi_gpu else outputs.loss
-    logits = outputs.logits
-    step += 1
-    labels2 = (labels != -100)
-    #pdb.set_trace()
-    indices = torch.arange(0,labels.shape[1])
-    
-    for k in range(labels2.shape[0]):
-        curl = labels2[k]
-        curlogit = logits[k,:,:].to('cpu')
-        curprep = perplexity(inputs["input_ids"][k].to('cpu'),curlogit,para_matrix)
-        prep_storge.append(curprep)
-        if curl.sum()>0:
-            curind = indices[curl]
+    ratio = (gg+1)*args.batchsize/total_samplers
+    if args.modelname == 'gpt2':
+        inputs,labels = tokenizer.tokenize(data, max_length=100)
+        if str(device)=='cuda':
+            inputs, lables = inputs.to(device), labels.to(device)
+        outputs = model.forward(input_ids = inputs)
+        if args.islastone:
+            last_logits, last_labels, loss = cllal(outputs, labels)
+            top5 += top_accuracy(last_logits, last_labels, 5)
+            top3 += top_accuracy(last_logits, last_labels, 3)
+            top1 += top_accuracy(last_logits, last_labels, 1)
+            tot += last_labels.numel()
+            if gg%(args.showstep)==99:
+                time1 = time.time()
+                logger.info('\t batch = %d \t complete = %.3f%% \t loss = %.3f \
+                            \t top1 = %.1f%% \t top3 = %.1f%% \t top5 = %.1f%% \
+                            \t cost_time = %.1fs\n'.replace('  ','')%(
+                            gg,ratio*100,loss.item(),top1*100/tot,top3*100/tot,
+                            top5*100/tot,time1-time0))
+                tb_writer.add_scalar('%s-lastone-loss'%args.modelname,loss.item(),gg)
+                tb_writer.add_scalar('%s-lastone-top1'%args.modelname,top1/tot,gg)
+                tb_writer.add_scalar('%s-lastone-top3'%args.modelname,top3/tot,gg)
+                tb_writer.add_scalar('%s-lastone-top5'%args.modelname,top5/tot,gg)
+                tb_writer.close()
         else:
-            continue
-        mask_token_logits = logits[k,curind,:]
-        real_idx = labels[k][labels[k]!=-100]
-        correct_5 += top_accuracy(mask_token_logits,real_idx,k=5)
-        correct_3 += top_accuracy(mask_token_logits,real_idx,k=3)
-        correct_1 += top_accuracy(mask_token_logits,real_idx,k=1)
+            if gg%args.showstep==99:
+                time1 = time.time()
+                loss, accuracy = calculate_loss_and_accuracy(outputs, labels=labels)
+                logger.info('\t batch = %d \t complete = %.3f%% \t loss = %.3f \
+                              \t acc = %.2f%% \t cost_time=%.1fs'.replace('  ','')%(
+                            gg,ratio*100,loss.item(),accuracy*100,time1-time0))
+                tb_writer.add_scalar('%s-testloss',loss.item(),gg)
+                tb_writer.add_scalar('%s-testacc',accuracy,gg)
+                tb_writer.close()
+    elif args.modelname == 'bert':
+        if args.islastone:
+            inputs, labels = tokenizer.tokenize(data, max_length=100, p_mask = 0, islastone=True)
+            inputs = inputs.to(device) if str(device)=='cuda' else inputs
+            labels = labels.to(device) if str(device)=='cuda' else labels
+        else:
+            inputs, labels = tokenizer.tokenize(data, max_length=100, p_mask = 0.15)
+            inputs = inputs.to(device) if str(device)=='cuda' else inputs
+            labels = labels.to(device) if str(device)=='cuda' else labels
+        outputs = model(**inputs, labels=labels)
+        loss = outputs.loss.mean() if multi_gpu else outputs.loss
+        masked_label = labels[labels != -100]
+        #pdb.set_trace()
+        masked_pre5 = outputs.logits[labels != -100].topk(5).indices
+        top5 += sum([masked_label[i].item() in masked_pre5[i] for i in range(len(masked_label))])
+        top3 += sum([masked_label[i].item() in masked_pre5[i,:3] for i in range(len(masked_label))])
+        top1 += sum([masked_label[i].item() in masked_pre5[i,:1] for i in range(len(masked_label))])
+        tot += masked_label.numel()
+        if gg%args.showstep==99:
+            time1 = time.time()
+            logger.info('\t batch = %d \t complete = %.1f%% \t loss = %.3f \t top1 = %.1f%% \t top3 = %.1f%%'
+                        '\t top5 = %.1f%% \t cost_time = %.1fs'%(gg,ratio*100,loss.item(),top1*100/tot,
+                          top3*100/tot,top5*100/tot,time1-time0))
+            tb_writer.add_scalar('%s-testloss'%args.modelname,loss.item(),gg)
+            tb_writer.add_scalar('%s-testtop1'%args.modelname,top1/tot,gg)
+            tb_writer.add_scalar('%s-testtop3'%args.modelname,top3/tot,gg)
+            tb_writer.add_scalar('%s-testtop5'%args.modelname,top5/tot,gg)
+            tb_writer.close()
+            
+        vinput = tokenizer.tokenizer(data,return_tensors="pt",max_length=100,
+                                     padding=True,truncation=True)
+        vout = model(**vinput)
+        vlogits = vout.logits.mean(1)  # batch * L * V -> batch * V
+        # PCA.fit(vlogits.detach().numpy())
+        # print(PCA.explained_variance_ratio_.sum())
+        sen_vector.append(vlogits)
+        sen_label.append(data)
+        if gg>1000:
+            break
+# svector = np.vstack(sen_vector)
 
-    total += torch.sum(labels2).item()
-    top5 = correct_5/total
-    top3 = correct_3/total
-    top1 = correct_1/total
-    mean_prep = sum(prep_storge[-1*args.batch_size:])/args.batch_size
-    if gg%(args.each_steps)==0:
-        time1 = time.time()
-        ratio = (gg+1)*args.batch_size/total_samplers
-        logger.info('\t batch = %d \t complete = %.3f%% \t loss = %.3f \t top1 = %.1f%% \t top3 = %.1f%% \n\t top5 = %.1f%% \t mean_log_preplexity = %.1f \t cost_time = %.1fs\n'%(gg,ratio*100,loss.item(),top1*100,top3*100,top5*100,mean_prep,time1-time0))
-    
-with open('./log_preplexity.txt','w',encoding='utf8') as f:
-    f.write(str(prep_storge))
-    
+svector = torch.stack([j for i in sen_vector for j in i])
+PCA.fit(svector.detach().numpy())
+print(PCA.explained_variance_ratio_.sum())
+svector2 = PCA.transform(svector.detach().numpy())
+slabel = [j for i in sen_label for j in i]
+
+svector2 = svector2[:200]
+slabel = slabel[:200]
+tb_writer.add_embedding(mat=svector2,metadata=slabel,global_step=1)
+tb_writer.close()
 
 
+#%%  
 
-
-
+data = np.load('./蚂蚁金服.npy')
+txt = '/Users/desktop/dataset/句子对数据/蚂蚁金服m.txt'
+with open('/Users/shizi/desktop/dataset/句子对数据/蚂蚁金服m.txt','r') as f:
+    data_l = f.read()
+    data_l = data_l.split('/n')
 
 
 
